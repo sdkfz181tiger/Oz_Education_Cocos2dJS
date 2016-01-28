dbb0520dd097f3bcdd270b9cc8d7b1eafe87819d
ozakishinichi <sdkfz181tiger@gmail.com>
2016-01-28 11:15:46 +0900

    リセット

--- HelloCocos2dJS/src/app.js
+++ HelloCocos2dJS/src/app.js
@@ -2,22 +2,12 @@
 var HelloWorldLayer = cc.Layer.extend({
 
 	self:null,
-	dispSize:null,
-	backSprite:null,
 	
 	ctor:function () {
 		this._super();
 	
 		// Self
 		self = this;
-		
-		// ディスプレイサイズ
-		dispSize = cc.director.getWinSize();
-		
-		// 背景画像
-		backSprite = new cc.Sprite(res.Bkg_0_png);
-		backSprite.setPosition(cc.p(dispSize.width / 2, dispSize.height / 2));
-		this.addChild(backSprite, 0);
 	}
 });
 
